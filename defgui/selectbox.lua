85012a364f6494f99bf0118ea513189fbed5d58b
Lutz Harder <lutz.harder@freenet.ag>
2020-03-27 15:49:49 +0100

    selectbox can open / close

--- defgui/selectbox.lua
+++ defgui/selectbox.lua
@@ -15,19 +15,22 @@ function Item.new( caption, name )
 end
 
 local Selectbox = {}
-function Selectbox.new( form, id, x, y, width, height, selecthandler )
+function Selectbox.new( form, id, x, y, width, height, myselecthandler )
+	
 	local openhandler = function( guiSelf, field, action_id, action )
 		if action_id == hash( "touch" ) and action.pressed then 
 			if gui.pick_node( field.rootNode, action.x, action.y ) then
-				print( "Open..." )
+				pprint( "open: " .. tostring( field.isOpen ) )
+
+				field:showList( not field.isOpen )
 			end
 		end
 	end
 	
 	local field = Field.new( id, x, y, width, height, openhandler )
 	field.items = {}
-	field.yOffset = 0
 	field.selectedIndex = 0
+	field.isOpen = false
 
 	local tmplNode = lua.guiGetNode( "selectbox/root" )
 	assert( tmplNode, "You must have a node in your GUI which must be declared as a template for selectboxes in your form!" )
@@ -43,14 +46,14 @@ function Selectbox.new( form, id, x, y, width, height, selecthandler )
 	field.openerTxt = nodes[ hash( "selectbox/txt" ) ]
 	assert( field.openerTxt, "Unable to access newly created selectbox/opener/txt node!" )
 	
-	field.list = nodes[ hash( "selectbox/list" ) ]
-	assert( field.items, "Unable to access newly created selectbox/list node!" )
-	
 	field.selected = nodes[ hash( "selectbox/selected" ) ]
 	assert( field.selected, "Unable to access newly created selectbox/selected node!" )
 	
 	field.clip = nodes[ hash( "selectbox/clip" ) ]
 	assert( field.clip, "Unable to access newly created selectbox/clip node!" )
+
+	field.entryTmpl = nodes[ hash( "selectbox/entry" ) ]
+	assert( field.entryTmpl, "Unable to access newly created selectbox/entry node!" )
 	
 	gui.set_id( field.opener, id .. "/opener" )
 	local openerSize = gui.get_size( field.opener )
@@ -71,22 +74,68 @@ function Selectbox.new( form, id, x, y, width, height, selecthandler )
 	gui.set_size( field.selected, vmath.vector3( width - 8, openerSize.y - 4, 1 ) )
 	gui.set_enabled( field.selected, true )
 	
-	gui.set_id( field.list, id .. "/list" )
-	gui.set_position( field.list, vmath.vector3( 0, -1 * openerSize.y - 1, 0 ) )
-	gui.set_size( field.list, vmath.vector3( width, height - openerSize.y, 1 ) )
-	gui.set_enabled( field.list, true )
-	
 	gui.set_id( field.clip, id .. "/clip" )
 	gui.set_position( field.clip, vmath.vector3( 0, -1 * openerSize.y - 1, 0 ) )
 	gui.set_size( field.clip, vmath.vector3( width, height - openerSize.y, 1 ) )
-	gui.set_enabled( field.clip, true )
+	gui.set_enabled( field.clip, false )
+
+	gui.set_id( field.entryTmpl, id .. "/entry" )
+	gui.set_position( field.entryTmpl, vmath.vector3( 4, 0, 0 ) )
+	gui.set_enabled( field.entryTmpl, false )
 
 	form:add( field )
+	
+
+	function field:createItemList()
+		pprint( "createList" )
+		
+		local txtHeight = nodeTextSize( field.entryTmpl, "o" ).height
+		local yDelta = txtHeight + 4 
+		local yDirection = -1
+		local openerSize = gui.get_size( field.opener )
+		local yOffset = -yDelta + 1
+		
+		gui.set_size( field.entryTmpl, vmath.vector3( field.width - 8, yDelta, 1 ) )
+
+		field.itemNodes = {}
+		local pos = nil
+		for i, item in ipairs( field.items ) do
+			pos = vmath.vector3( 4, -1 * yOffset + ( yDirection * yDelta * i ), 0 )
+			
+			field.itemNodes[ i ] = gui.clone( field.entryTmpl )
+			gui.set_position( field.itemNodes[ i ], pos )
+			gui.set_text( field.itemNodes[ i ], item.caption )
+			gui.set_enabled( field.itemNodes[ i ], false )
+		end
+	end
+
+
+	function field:showList( state )
+		pprint( "showList" )
+		
+		if state == nil then state = true end
+
+		for i, node in ipairs( field.itemNodes ) do
+			gui.set_enabled( field.itemNodes[ i ], state )
+		end
+		gui.set_enabled( field.clip, state )
+
+		field.isOpen = state
+	end
 	
 	
 	function field:textSize( txt )
 		if txt == nil then txt = field.value end
 		return nodeTextSize( field.rootNode, txt )
+	end
+
+
+	function field:setFont( fontname )
+		gui.set_font( field.txtNode, fontname )
+		gui.set_font( field.cursorNode, fontname )
+
+		-- need to adjust reference width of chars
+		field.charWidth = field:textSize( "o" ).width
 	end
 	
 
@@ -98,7 +147,7 @@ function Selectbox.new( form, id, x, y, width, height, selecthandler )
 	end
 
 	
-	function field:addItem( caption, value )
+	function field:addItem( caption, value, doNotCreateNewList )
 		local item = Item.new( caption, value )
 		table.insert( field.items, item )
 
@@ -106,13 +155,33 @@ function Selectbox.new( form, id, x, y, width, height, selecthandler )
 		if field.selectedIndex == 0 then 
 			field:select( 1 ) 
 		end
+
+		if not doNotCreateNewList then 
+			field:createItemList()
+		end
+	end
+
+
+	function field:setItems( items )
+		assert( items, "Items muzst not be nil!" )
+
+		local item = nil
+		for i, v in ipairs( items ) do
+			if type( v ) == "string" then 
+				item = Item.new( v )
+			else 
+				item = v
+			end
+			field:addItem( item.caption, item.value, true )
+		end
+
+		-- create list only once, more efficient
+		field:createItemList()
 	end
 
 	
 	return field
 end
-
-
 
 
 return Selectbox
